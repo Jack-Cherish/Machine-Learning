b03e98d553f2a07c79103b84ceb6cd0832e7c515
Jack-Cherish <c411184003@gmail.com>
2017-07-21 16:47:25 +0800

    21 July 2017:Decision Tree

--- Decision Tree/Decision Tree.py
+++ Decision Tree/Decision Tree.py
@@ -1,43 +1,31 @@
 # -*- coding: UTF-8 -*-
 from math import log
 
-
 """
-函数说明:计算给定数据集的香农熵
+函数说明:计算给定数据集的经验熵(香农熵)
 
 Parameters:
 	dataSet - 数据集
 Returns:
-	shannonEnt - 香农熵
+	shannonEnt - 经验熵(香农熵)
 Author:
 	Jack Cui
 Modify:
 	2017-03-29
 """
 def calcShannonEnt(dataSet):
-	#返回数据集的行数
-	numEntires = len(dataSet)
-	#保存每个标签(Label)出现次数的字典
-	labelCounts = {}
-	#对每组特征向量进行统计
-	for featVec in dataSet:
-		#提取标签(Label)信息
-		currentLabel = featVec[-1]
-		#如果标签(Label)没有放入统计次数的字典,添加进去
-		if currentLabel not in labelCounts.keys():
+	numEntires = len(dataSet)						#返回数据集的行数
+	labelCounts = {}								#保存每个标签(Label)出现次数的字典
+	for featVec in dataSet:							#对每组特征向量进行统计
+		currentLabel = featVec[-1]					#提取标签(Label)信息
+		if currentLabel not in labelCounts.keys():	#如果标签(Label)没有放入统计次数的字典,添加进去
 			labelCounts[currentLabel] = 0
-		#Label计数
-		labelCounts[currentLabel] += 1
-	#香农熵
-	shannonEnt = 0.0
-	#计算香农熵
-	for key in labelCounts:
-		#选择该标签(Label)的概率
-		prob = float(labelCounts[key]) / numEntires
-		#利用公式计算
-		shannonEnt -= prob * log(prob, 2)
-	#返回香农熵
-	return shannonEnt
+		labelCounts[currentLabel] += 1				#Label计数
+	shannonEnt = 0.0								#经验熵(香农熵)
+	for key in labelCounts:							#计算香农熵
+		prob = float(labelCounts[key]) / numEntires	#选择该标签(Label)的概率
+		shannonEnt -= prob * log(prob, 2)			#利用公式计算
+	return shannonEnt								#返回经验熵(香农熵)
 
 """
 函数说明:创建测试数据集
@@ -46,23 +34,30 @@ Parameters:
 	无
 Returns:
 	dataSet - 数据集
-	features - 特征
+	labels - 分类属性
 Author:
 	Jack Cui
 Modify:
-	2017-03-29
+	2017-07-20
 """
 def createDataSet():
-	#数据集
-	dataSet = [[1, 1, 'yes'],
-			[1, 1, 'yes'],
-			[1, 0, 'no'],
-			[0, 1, 'no'],
-			[0, 1, 'no']]
-	#特征
-	features = ['no surfacing', 'flippers']
-	#返回数据集和标签
-	return dataSet, features
+	dataSet = [[0, 0, 0, 0, 'no'],						#数据集
+			[0, 0, 0, 1, 'no'],
+			[0, 1, 0, 1, 'yes'],
+			[0, 1, 1, 0, 'yes'],
+			[0, 0, 0, 0, 'no'],
+			[1, 0, 0, 0, 'no'],
+			[1, 0, 0, 1, 'no'],
+			[1, 1, 1, 1, 'yes'],
+			[1, 0, 1, 2, 'yes'],
+			[1, 0, 1, 2, 'yes'],
+			[2, 0, 1, 2, 'yes'],
+			[2, 0, 1, 1, 'yes'],
+			[2, 1, 0, 1, 'yes'],
+			[2, 1, 0, 2, 'yes'],
+			[2, 0, 0, 0, 'no']]
+	labels = ['不放贷', '放贷']			#分类属性
+	return dataSet, labels 							#返回数据集和分类属性
 
 """
 函数说明:按照给定特征划分数据集
@@ -78,73 +73,48 @@ Author:
 Modify:
 	2017-03-30
 """
-def splitDataSet(dataSet, axis, value):
-	#创建返回的数据集列表
-	retDataSet = []
-	#遍历数据集
-	for featVec in dataSet:
+def splitDataSet(dataSet, axis, value):		
+	retDataSet = []										#创建返回的数据集列表
+	for featVec in dataSet: 							#遍历数据集
 		if featVec[axis] == value:
-			#去掉axis特征
-			reducedFeatVec = featVec[:axis]
-			reducedFeatVec.extend(featVec[axis+1:])
-			#将符合条件的添加到返回的数据集
+			reducedFeatVec = featVec[:axis]				#去掉axis特征
+			reducedFeatVec.extend(featVec[axis+1:]) 	#将符合条件的添加到返回的数据集
 			retDataSet.append(reducedFeatVec)
-	#返回划分后的数据集
-	return retDataSet
+	return retDataSet		  							#返回划分后的数据集
 
 """
-函数说明:选择最好的数据集划分方式
+函数说明:选择最优特征
 
 Parameters:
 	dataSet - 数据集
 Returns:
-	bestFeature - 最好划分数据集的特征的索引值
+	bestFeature - 信息增益最大的(最优)特征的索引值
 Author:
 	Jack Cui
 Modify:
 	2017-03-30
 """
 def chooseBestFeatureToSplit(dataSet):
-	#特征数量
-	numFeatures = len(dataSet[0]) - 1
-	#计算数据集的香农熵
-	baseEntropy = calcShannonEnt(dataSet)
-	#信息增益
-	bestInfoGain = 0.0
-	#划分特征的索引值
-	bestFeature = -1
-	#遍历所有特征
-	for i in range(numFeatures):
-		#获取dataSet的第i个所有特征,当i=0时,featList=[1,1,1,0,0],当i=1时,featList=[1,1,0,1,1]
+	numFeatures = len(dataSet[0]) - 1					#特征数量
+	baseEntropy = calcShannonEnt(dataSet) 				#计算数据集的香农熵
+	bestInfoGain = 0.0  								#信息增益
+	bestFeature = -1									#最优特征的索引值
+	for i in range(numFeatures): 						#遍历所有特征
+		#获取dataSet的第i个所有特征
 		featList = [example[i] for example in dataSet]
-		#创建set集合{},元素不可重复
-		uniqueVals = set(featList)
-		#新的香农熵
-		newEntropy = 0.0
-		#计算新的香农熵
-		for value in uniqueVals:
-			#subDataSet划分后的子集
-			subDataSet = splitDataSet(dataSet, i, value)
-			#计算概率
-			prob = len(subDataSet) / float(len(dataSet))
-			#根据公式计算香农熵
-			newEntropy += prob * calcShannonEnt(subDataSet)
-		#信息增益
-		infoGain = baseEntropy - newEntropy
-		#计算最好的信息增益
-		if (infoGain > bestFeature):
-			#更新最好的信息增益
-			bestInfoGain = infoGain
-			#更新最好划分数据集的特征的索引值
-			bestFeature = i
-	#返回最好划分数据集的特征的索引值
-	return bestFeature
-
-
-
+		uniqueVals = set(featList)     					#创建set集合{},元素不可重复
+		newEntropy = 0.0  								#经验条件熵
+		for value in uniqueVals: 						#计算信息增益
+			subDataSet = splitDataSet(dataSet, i, value) 		#subDataSet划分后的子集
+			prob = len(subDataSet) / float(len(dataSet))   		#计算子集的概率
+			newEntropy += prob * calcShannonEnt(subDataSet) 	#根据公式计算经验条件熵
+		infoGain = baseEntropy - newEntropy 					#信息增益
+		print("第%d个特征的增益为%.3f" % (i, infoGain))			#打印每个特征的信息增益
+		if (infoGain > bestInfoGain): 							#计算信息增益
+			bestInfoGain = infoGain 							#更新信息增益，找到最大的信息增益
+			bestFeature = i 									#记录信息增益最大的特征的索引值
+	return bestFeature 											#返回信息增益最大的特征的索引值
 
 if __name__ == '__main__':
 	dataSet, features = createDataSet()
-	print(dataSet)
-	bestFeature = chooseBestFeatureToSplit(dataSet)
-	print(bestFeature)
+	print("最优特征索引值:" + str(chooseBestFeatureToSplit(dataSet)))
