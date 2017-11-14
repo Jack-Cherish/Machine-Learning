066a2ec321b7e73b777212d637df8f78fa95392c
DanielLeeGit <706321263@qq.com>
2017-11-14 21:25:53 +0800

    Simplify decision tree.

--- Decision Tree/Decision Tree.py
+++ Decision Tree/Decision Tree.py
@@ -2,6 +2,7 @@
 from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 from math import log
+from collections import Counter
 import operator
 import pickle
 
@@ -17,21 +18,20 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use list comprehension to simplify for.
 	2017-07-24
 """
 def calcShannonEnt(dataSet):
-	numEntires = len(dataSet)						#返回数据集的行数
-	labelCounts = {}								#保存每个标签(Label)出现次数的字典
-	for featVec in dataSet:							#对每组特征向量进行统计
-		currentLabel = featVec[-1]					#提取标签(Label)信息
-		if currentLabel not in labelCounts.keys():	#如果标签(Label)没有放入统计次数的字典,添加进去
-			labelCounts[currentLabel] = 0
-		labelCounts[currentLabel] += 1				#Label计数
-	shannonEnt = 0.0								#经验熵(香农熵)
-	for key in labelCounts:							#计算香农熵
-		prob = float(labelCounts[key]) / numEntires	#选择该标签(Label)的概率
-		shannonEnt -= prob * log(prob, 2)			#利用公式计算
-	return shannonEnt								#返回经验熵(香农熵)
+	# Count labels
+	label_count = Counter(data[-1] for data in dataSet)
+	# Compute prob
+	probs = [p[1] / len(dataSet) for p in label_count.items()]
+	# Compute entropy and sum
+	shannonEnt = sum([-p * log(p, 2) for p in probs])
+	return shannonEnt
 
 """
 函数说明:创建测试数据集
@@ -81,15 +81,15 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		It is not useful to delete some data, 
+		so use list comprehension is more readable.
 	2017-07-24
 """
-def splitDataSet(dataSet, axis, value):		
-	retDataSet = []										#创建返回的数据集列表
-	for featVec in dataSet: 							#遍历数据集
-		if featVec[axis] == value:
-			reducedFeatVec = featVec[:axis]				#去掉axis特征
-			reducedFeatVec.extend(featVec[axis+1:]) 	#将符合条件的添加到返回的数据集
-			retDataSet.append(reducedFeatVec)
+def splitDataSet(dataSet, axis, value):
+	retDataSet = [data for data in dataSet for i, v in enumerate(data) if i == axis and v == value]
 	return retDataSet		  							#返回划分后的数据集
 
 """
@@ -104,28 +104,28 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use list comprehension and sum to make the code more clear.
 	2017-07-20
 """
 def chooseBestFeatureToSplit(dataSet):
-	numFeatures = len(dataSet[0]) - 1					#特征数量
-	baseEntropy = calcShannonEnt(dataSet) 				#计算数据集的香农熵
-	bestInfoGain = 0.0  								#信息增益
-	bestFeature = -1									#最优特征的索引值
-	for i in range(numFeatures): 						#遍历所有特征
-		#获取dataSet的第i个所有特征
-		featList = [example[i] for example in dataSet]
-		uniqueVals = set(featList)     					#创建set集合{},元素不可重复
-		newEntropy = 0.0  								#经验条件熵
-		for value in uniqueVals: 						#计算信息增益
-			subDataSet = splitDataSet(dataSet, i, value) 		#subDataSet划分后的子集
-			prob = len(subDataSet) / float(len(dataSet))   		#计算子集的概率
-			newEntropy += prob * calcShannonEnt(subDataSet) 	#根据公式计算经验条件熵
-		infoGain = baseEntropy - newEntropy 					#信息增益
-		# print("第%d个特征的增益为%.3f" % (i, infoGain))			#打印每个特征的信息增益
-		if (infoGain > bestInfoGain): 							#计算信息增益
-			bestInfoGain = infoGain 							#更新信息增益，找到最大的信息增益
-			bestFeature = i 									#记录信息增益最大的特征的索引值
-	return bestFeature 											#返回信息增益最大的特征的索引值
+	base_entropy = calcShannonEnt(dataSet)      #计算数据集的香农熵
+	best_info_gain = 0
+	best_feature = -1
+	for i in range(len(dataSet[0]) - 1):    	# 遍历所有特征
+		feature_count = Counter([data[i] for data in dataSet])
+		# 计算信息增益
+		new_entropy = sum(feature[1] / float(len(dataSet)) * calcShannonEnt(splitDataSet(dataSet, i, feature[0])) \
+						  for feature in feature_count.items())
+		# 信息增益
+		info_gain = base_entropy - new_entropy
+		# print('No. {0} feature info gain is {1:.3f}'.format(i, info_gain))
+		if info_gain > best_info_gain:
+			best_info_gain = info_gain
+			best_feature = i
+	return best_feature
 
 
 """
@@ -140,15 +140,15 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use Counter to clear code.
 	2017-07-24
 """
 def majorityCnt(classList):
-	classCount = {}
-	for vote in classList:										#统计classList中每个元素出现的次数
-		if vote not in classCount.keys():classCount[vote] = 0	
-		classCount[vote] += 1
-	sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)		#根据字典的值降序排序
-	return sortedClassCount[0][0]								#返回classList中出现次数最多的元素
+	major_label = Counter(classList).most_common(1)[0]
+	return major_label
 
 """
 函数说明:创建决策树
@@ -195,6 +195,10 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use `is` instead of __name__ == 'dict'
 	2017-07-24
 """
 def getNumLeafs(myTree):
@@ -202,9 +206,10 @@ def getNumLeafs(myTree):
     firstStr = next(iter(myTree))								#python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
     secondDict = myTree[firstStr]								#获取下一组字典
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':				#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+        if type(secondDict[key]) is dict:				#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
             numLeafs += getNumLeafs(secondDict[key])
-        else:   numLeafs +=1
+        else:
+            numLeafs += 1
     return numLeafs
 
 """
@@ -219,6 +224,11 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use `is` instead of __name__ == 'dict'
+		Max is better than if... .
 	2017-07-24
 """
 def getTreeDepth(myTree):
@@ -226,10 +236,11 @@ def getTreeDepth(myTree):
     firstStr = next(iter(myTree))								#python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
     secondDict = myTree[firstStr]								#获取下一个字典
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':				#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+        if type(secondDict[key]) is dict:				#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
             thisDepth = 1 + getTreeDepth(secondDict[key])
-        else:   thisDepth = 1
-        if thisDepth > maxDepth: maxDepth = thisDepth			#更新层数
+        else:
+            thisDepth = 1
+        maxDepth = max(maxDepth ,thisDepth)			#更新层数
     return maxDepth
 
 """
@@ -272,8 +283,8 @@ Modify:
 	2017-07-24
 """ 
 def plotMidText(cntrPt, parentPt, txtString):
-	xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]											#计算标注位置					
-	yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+	xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]											#计算标注位置
+	yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
 	createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
 
 """
@@ -290,6 +301,10 @@ Author:
 Blog:
 	http://blog.csdn.net/c406495762
 Modify:
+    2017-11-09 by Cugtyt 
+		* GitHub(https://github.com/Cugtyt) 
+		* Email(cugtyt@qq.com)
+		Use `is` instead of __name__ == 'dict'
 	2017-07-24
 """ 
 def plotTree(myTree, parentPt, nodeTxt):
@@ -298,19 +313,19 @@ def plotTree(myTree, parentPt, nodeTxt):
 	numLeafs = getNumLeafs(myTree)  														#获取决策树叶结点数目，决定了树的宽度
 	depth = getTreeDepth(myTree)															#获取决策树层数
 	firstStr = next(iter(myTree))															#下个字典     											
-	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)	#中心位置
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)	#中心位置
 	plotMidText(cntrPt, parentPt, nodeTxt)													#标注有向边属性值
 	plotNode(firstStr, cntrPt, parentPt, decisionNode)										#绘制结点
 	secondDict = myTree[firstStr]															#下一个字典，也就是继续绘制子结点
-	plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD										#y偏移
+	plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD										#y偏移
 	for key in secondDict.keys():								
-		if type(secondDict[key]).__name__=='dict':											#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+		if type(secondDict[key]) is dict:											#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
 			plotTree(secondDict[key],cntrPt,str(key))        								#不是叶结点，递归调用继续绘制
 		else:																				#如果是叶结点，绘制叶结点，并标注有向边属性值 											
-			plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+			plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
 			plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
 			plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
-	plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+	plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
 
 """
 函数说明:创建绘制面板
@@ -333,8 +348,9 @@ def createPlot(inTree):
     createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)    							#去掉x、y轴
     plotTree.totalW = float(getNumLeafs(inTree))											#获取决策树叶结点数目
     plotTree.totalD = float(getTreeDepth(inTree))											#获取决策树层数
-    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;								#x偏移
-    plotTree(inTree, (0.5,1.0), '')															#绘制决策树
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0								#x偏移
+    plotTree(inTree, (0.5, 1.0), '')															#绘制决策树
     plt.show()																				#显示绘制结果
 
 """
@@ -359,9 +375,10 @@ def classify(inputTree, featLabels, testVec):
 	featIndex = featLabels.index(firstStr)												
 	for key in secondDict.keys():
 		if testVec[featIndex] == key:
-			if type(secondDict[key]).__name__ == 'dict':
+			if type(secondDict[key]) is dict:
 				classLabel = classify(secondDict[key], featLabels, testVec)
-			else: classLabel = secondDict[key]
+			else:
+				classLabel = secondDict[key]
 	return classLabel
 
 """
--- Decision Tree/Sklearn-Decision Tree.py
+++ Decision Tree/Sklearn-Decision Tree.py
@@ -3,7 +3,6 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.externals.six import StringIO
 from sklearn import tree
 import pandas as pd
-import numpy as np
 import pydotplus
 
 if __name__ == '__main__':
@@ -24,21 +23,38 @@ if __name__ == '__main__':
 		lenses_list = []
 	# print(lenses_dict)														#打印字典信息
 	lenses_pd = pd.DataFrame(lenses_dict)									#生成pandas.DataFrame
-	# print(lenses_pd)														#打印pandas.DataFrame
-	le = LabelEncoder()														#创建LabelEncoder()对象，用于序列化			
+	print(lenses_pd)														#打印pandas.DataFrame
+	le = LabelEncoder()														#创建LabelEncoder()对象，用于序列化
 	for col in lenses_pd.columns:											#序列化
 		lenses_pd[col] = le.fit_transform(lenses_pd[col])
 	# print(lenses_pd)														#打印编码信息
+
+    # ---------------- Second way -------------------------
+	# 2017-11-09 by Cugtyt
+	# 	* GitHub(https://github.com/Cugtyt)
+	# 	* Email(cugtyt@qq.com)
+	# 	pandas can directly read file, so this way is more easy.
+	#
+	#
+	# lenses_pd = pd.DataFrame(pd.read_table('lenses.txt'))
+	# lenses_pd.columns = ['age', 'prescript', 'astigmatic', 'tearRate', 'target']
+	# lenses_pd = lenses_pd.drop(['target'], axis=1)
+	# print(lenses_pd)
+	# le = LabelEncoder()
+	# for col in lenses_pd.columns:
+	# 	lenses_pd[col] = le.fit_transform(lenses_pd[col])
+	# print(lenses_pd)
+    # ----------------------------------------------------
 
 	clf = tree.DecisionTreeClassifier(max_depth = 4)						#创建DecisionTreeClassifier()类
 	clf = clf.fit(lenses_pd.values.tolist(), lenses_target)					#使用数据，构建决策树
 
 	dot_data = StringIO()
 	tree.export_graphviz(clf, out_file = dot_data,							#绘制决策树
-						feature_names = lenses_pd.keys(),
-						class_names = clf.classes_,
-						filled=True, rounded=True,
-						special_characters=True)
+						 feature_names = lenses_pd.keys(),
+						 class_names = clf.classes_,
+						 filled=True, rounded=True,
+						 special_characters=True)
 	graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
 	graph.write_pdf("tree.pdf")												#保存绘制好的决策树，以PDF的形式存储。
 
