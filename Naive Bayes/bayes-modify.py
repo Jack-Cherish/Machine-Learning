ea0c9b1e8363b4dfb98e1ceef2fa84d2834d017c
wk <wangkechn@163.com>
2019-03-01 11:15:01 +0800

    Update bayes-modify.py

--- Naive Bayes/bayes-modify.py
+++ Naive Bayes/bayes-modify.py
@@ -145,7 +145,8 @@ Modify:
     2017-08-14
 """
 def textParse(bigString):                                                   #将字符串转换为字符列表
-    listOfTokens = re.split(r'\W*', bigString)                              #将特殊符号作为切分标志进行字符串切分，即非字母、非数字
+    # * 会匹配0个或多个规则，split会将字符串分割成单个字符【python3.5+】; 这里使用\W 或者\W+ 都可以将字符数字串分割开，产生的空字符将会在后面的列表推导式中过滤掉
+    listOfTokens = re.split(r'\W+', bigString)                              #将特殊符号作为切分标志进行字符串切分，即非字母、非数字
     return [tok.lower() for tok in listOfTokens if len(tok) > 2]            #除了单个字母，例如大写的I，其它单词变成小写
 
 """
